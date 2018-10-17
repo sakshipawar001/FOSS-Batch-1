aa6b95739df21cbc3d435b3922d19c2da73c1b7d
JadhavMehul <44131214+JadhavMehul@users.noreply.github.com>
2018-10-17 15:33:03 +0530

    Add files via upload

---
+++ addition of two number.py
@@ -0,0 +1,9 @@
+#addition of two number
+
+first_number=int(input("enter 1st number=")) #user input of first number
+second_number=int(input("enter 2nd number=")) #user input of second number
+
+total=(first_number + second_number) #formula for addition of two number
+
+
+print("the addition of ",first_number,"and ",second_number,"is ",total)
