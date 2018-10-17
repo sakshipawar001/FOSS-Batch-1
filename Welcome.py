0402c74a1029857943108109a1e65421be71214e
sakshipawar001 <44046769+sakshipawar001@users.noreply.github.com>
2018-10-17 14:47:01 +0530

    edited by sakshi pawar and gunita kasar

--- Welcome.py
+++ Welcome.py
@@ -1,2 +1,4 @@
 print("Welcome to Github Repository.")
-print("roll no. 17 &  05","prime no.")
+print ("roll no. 34 & 28","continue and break func.")
+print("Roll No. 21,12, 'print prime numbers in an interval '")
+print("Roll No 33,25, 'input function'")
