be7a628f32fb7e487919317c705d9feec76b51ca
ameypatole <44046909+ameypatole@users.noreply.github.com>
2018-10-17 02:36:21 -0700

    roll no.31,27 updated leap year program

--- Welcome.py
+++ Welcome.py
@@ -1,5 +1,4 @@
-anprint("Welcome to Github Repository.")
+print("Welcome to Github Repository.")
 print ("roll no. 34 & 28","continue and break func.")
 print("Roll No. 21,12, 'print prime numbers in an interval '")
-print("Roll number is 3 and 32, 'factorials.py'")
-print("roll number is 08 and 02,'while loop'")
+print("roll no.31,27","Leap year program")
