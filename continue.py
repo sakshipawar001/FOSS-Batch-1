56de1ce532a09dd1476016ba8db984a4ff79a62b
vaibhav1308 <44046844+vaibhav1308@users.noreply.github.com>
2018-10-17 15:29:18 +0530

    creates continue & break function

--- continue.py
+++ continue.py
@@ -1,23 +1,24 @@
+#break function
 number = 0
 
 for number in range(10):
    number = number + 1
 
    if number == 5:
-      break    # break here
+      break   
 
    print('Number is ' + str(number))
 
 print('Out of loop')
 
-
+#continue function
 number = 0
 
 for number in range(10):
    number = number + 1
 
    if number == 5:
-      continue    # continue here
+      continue  
 
    print('Number is ' + str(number))
 
