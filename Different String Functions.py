19c6052ea3471aee23f7d76779841928e83dc0f0
maneeshpranav <44046871+maneeshpranav@users.noreply.github.com>
2018-10-17 14:35:28 +0530

    added Different String Functions program

---
+++ Different String Functions.py
@@ -0,0 +1,20 @@
+# minimum()
+a=min(14,45,25,96)
+print(a)
+
+# maximum()
+a=max(14,45,25,96)
+print(a)
+
+#len()
+a=len("length")
+print(a)
+
+#int()
+a=int(5.6)
+print(a)
+
+#float()
+a=float(6)
+print(a)
+
