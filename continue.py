3791d3e838dc8301833a0e4959dd56bcab60a463
vaibhav1308 <44046844+vaibhav1308@users.noreply.github.com>
2018-10-17 14:21:39 +0530

    create continue.py

---
+++ continue.py
@@ -0,0 +1,24 @@
+number = 0
+
+for number in range(10):
+   number = number + 1
+
+   if number == 5:
+      break    # break here
+
+   print('Number is ' + str(number))
+
+print('Out of loop')
+
+
+number = 0
+
+for number in range(10):
+   number = number + 1
+
+   if number == 5:
+      continue    # continue here
+
+   print('Number is ' + str(number))
+
+print('Out of loop')
