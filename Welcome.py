1f66cfc4fcd7b8988c9a0f2da2ed3b4cfb657986
VallabhAryan <44046997+VallabhAryan@users.noreply.github.com>
2018-10-17 02:20:09 -0700

    roll no 36,23 updated if else program

--- Welcome.py
+++ Welcome.py
@@ -1,4 +1,6 @@
 print("Welcome to Github Repository.")
 print ("roll no. 34 & 28","continue and break func.")
 print("Roll No. 21,12, 'print prime numbers in an interval '")
-print("roll no.01,16"."print pyhton palindron")
+print("Roll No 33,25, 'input function'")
+print("Roll No. 11 & 29 ","added D!fferent $tring Functions program").
+print("roll no.36,23","If-Else program")
