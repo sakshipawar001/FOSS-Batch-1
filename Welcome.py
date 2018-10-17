4e0691f00cba35bb5ac10b93e975bb1b448a9778
maneeshpranav <44046871+maneeshpranav@users.noreply.github.com>
2018-10-17 14:47:53 +0530

    Roll NO. 11 & 29 added Different String Function Program

--- Welcome.py
+++ Welcome.py
@@ -2,3 +2,4 @@ print("Welcome to Github Repository.")
 print ("roll no. 34 & 28","continue and break func.")
 print("Roll No. 21,12, 'print prime numbers in an interval '")
 print("Roll No 33,25, 'input function'")
+print("Roll No. 11 & 29 ","added D!fferent $tring Functions program").
