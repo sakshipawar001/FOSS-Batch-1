0bc8ef3a71dc8f7e19464526ea17882c178ab05d
janita2802 <44046907+janita2802@users.noreply.github.com>
2018-10-17 14:43:29 +0530

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -1,3 +1,4 @@
 print("Welcome to Github Repository.")
 print ("roll no. 34 & 28","continue and break func.")
 print("Roll No. 21,12, 'print prime numbers in an interval '")
+print("Roll no."35,"sum of prime numbers")
